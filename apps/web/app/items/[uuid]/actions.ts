e4349bbc9646a9a057ff679846cef40bf3f9c5e2
Keilyn Rodríguez Perojo <73491750+keilynrp@users.noreply.github.com>
2026-08-15 22:21:46 -0600

    Accept linguistic variant in draft actions

--- apps/web/app/items/[uuid]/actions.ts
+++ apps/web/app/items/[uuid]/actions.ts
@@ -14,6 +14,7 @@ const LINGUISTIC_FIELDS = [
   "dc.subject.linguisticFamily",
   "dc.subject.linguisticBranch",
   "dc.subject.linguiscgroup",
+  "dc.subject.linguisticVariant",
   "dc.description.registeredLanguage",
 ] as const;
 const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
